1cc5c23db72aada4a6d280edb368cd7e8e1a3240
isaac <isaach2026@student.cis.edu.hk>
2023-11-17 12:04:30 +0800

    dotn know how to do 3 4

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
 public class Main {
 
     // 1. parenthesesCheck
+    public static boolean parenthesesCheck(String str) {
+        int count = 0;
 
-
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '(') {
+                count++;
+            } else if (c == ')') {
+                count--;
+            }
+            if (count < 0) {
+                return false;
+            }
+        }
+        return count == 0;
+    }
     // 2. reverseInteger
-
-
+    public static String reverseInteger(int n) {
+        String str = String.valueOf(n);
+        StringBuilder reversed = new StringBuilder(str).reverse();
+        return reversed.toString();
+    }
     // 3. encryptThis
 
 
